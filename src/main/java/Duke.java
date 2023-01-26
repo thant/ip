305cba16305571757cc28ea149912b813e541f91
LAPTOP-D5S3B81V\thant <thantaunghtet@gmail.com>
2023-01-26 15:16:21 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
+
 public class Duke {
+
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int count = 0;
 
             System.out.println("____________________________________________________________\n  " +
                     " Hello! I'm Duke\n" +
                     " What can I do for you?\n" +
                     "____________________________________________________________\n");
+
             while(true) {
                 Scanner newScanner = new Scanner(System.in);
                 String user_input = newScanner.nextLine();
@@ -17,19 +20,41 @@ public class Duke {
                             "____________________________________________________________\n");
                     break;
                 }
-                if (user_input.equals("list")){
-                    System.out.println("____________________________________________________________\n  ");
-                    for (int i=0; i < count; i++){
-                        System.out.println(i+1 + ". " + list[i]+ "\n");
+
+                else if (user_input.contains("mark")) {
+                    if (user_input.contains("unmark")) {
+                        char index = user_input.charAt(user_input.length()-1);
+                        int indexint = Character.getNumericValue(index) - 1;
+                        list[indexint].unmarkTask();
+                        System.out.println("____________________________________________________________\n" +
+                                "     OK, I've marked this task as not done yet:\n [ ] " + list[indexint].description + "\n ____________________________________________________________\n");
                     }
-                    System.out.println("____________________________________________________________\n  ");
+                    else{
+                        char index = user_input.charAt(user_input.length()-1);
+                        int indexint = Character.getNumericValue(index) - 1;
+                        list[indexint].markAsDone();
+                        System.out.println("____________________________________________________________\n" +
+                                "     Nice! I've marked this task as done:\n [X] " + list[indexint].description + "\n ____________________________________________________________\n");
+                    }
                 }
 
+                else if (user_input.equals("list")) {
+                    System.out.println("____________________________________________________________\n");
+                    for (int i = 0; i < count; i++) {
+                        if (i != count - 1) {
+                            System.out.println(i + 1 + ". [" + list[i].getStatusIcon() + "] " + list[i].description + "\n");
+                        } else {
+                            System.out.println(i + 1 + ". [" + list[i].getStatusIcon() + "] " + list[i].description);
+                        }
+                        System.out.println("____________________________________________________________\n");
+                    }
+                }
 
                 else {
+                    Task newtask = new Task(user_input);
                     System.out.println("____________________________________________________________\n  " + "added: " + user_input +
                             "\n____________________________________________________________\n");
-                    list[count] = user_input;
+                    list[count] = newtask;
                     count++;
                 }
             }
