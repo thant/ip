fc6b96d412f8c081c1a9eee850d8f86891c2e4ec
LAPTOP-D5S3B81V\thant <thantaunghtet@gmail.com>
2023-01-26 16:37:57 +0800

    Level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,15 +14,16 @@ public class Duke {
             while(true) {
                 Scanner newScanner = new Scanner(System.in);
                 String user_input = newScanner.nextLine();
+                String[] user_input_split = user_input.split(" ");
 
-                if (user_input.equals("bye") | user_input.equals("Bye")) {
+                if (user_input_split[0].equals("bye") | user_input_split[0].equals("Bye")) {
                     System.out.println(" Bye. Hope to see you again soon!\n" +
                             "____________________________________________________________\n");
                     break;
                 }
 
-                else if (user_input.contains("mark")) {
-                    if (user_input.contains("unmark")) {
+                else if (user_input_split[0].contains("mark")) {
+                    if (user_input_split[0].contains("unmark")) {
                         char index = user_input.charAt(user_input.length()-1);
                         int indexint = Character.getNumericValue(index) - 1;
                         list[indexint].unmarkTask();
@@ -38,18 +39,49 @@ public class Duke {
                     }
                 }
 
-                else if (user_input.equals("list")) {
+                else if (user_input_split[0].equals("list")) {
                     System.out.println("____________________________________________________________\n");
                     for (int i = 0; i < count; i++) {
-                        if (i != count - 1) {
-                            System.out.println(i + 1 + ". [" + list[i].getStatusIcon() + "] " + list[i].description + "\n");
-                        } else {
-                            System.out.println(i + 1 + ". [" + list[i].getStatusIcon() + "] " + list[i].description);
+                            System.out.println(i + 1 + "." + list[i].toString());
                         }
-                        System.out.println("____________________________________________________________\n");
-                    }
+                    System.out.println("____________________________________________________________\n");
                 }
 
+                else if (user_input_split[0].equals("todo")) {
+                    list[count] = new Todo(user_input.substring(5));
+                    count++;
+                    System.out.println("____________________________________________________________\n" +
+                            "     Got it. I've added this task:\n" +
+                            "       [T][ ] " + user_input.substring(5) +
+                            "\n     Now you have " + count + " tasks in the list.\n" +
+                            "____________________________________________________________");
+                }
+
+                else if (user_input_split[0].equals("deadline")) {
+                    String[] user_input_splitby = user_input.split("/by");
+
+                    list[count] = new Deadline(user_input_splitby[0].substring(8),user_input_splitby[1].substring(1));
+                    count++;
+                    System.out.println("____________________________________________________________\n" +
+                            "     Got it. I've added this task:\n" +
+                            "        " + list[count-1].toString() +
+                            "\n     Now you have " + count + " tasks in the list.\n" +
+                            "____________________________________________________________");
+                }
+
+                else if (user_input_split[0].equals("event")) {
+                    String[] user_input_splitfrom = user_input.split("/from");
+                    String[] user_input_splitto = user_input_splitfrom[1].split("/to");
+
+                    list[count] = new Event(user_input_splitfrom[0].substring(5),user_input_splitto[0].substring(1), user_input_splitto[1].substring(1));
+                    count++;
+                    System.out.println("____________________________________________________________\n" +
+                            "     Got it. I've added this task:\n" +
+                            "        " + list[count-1].toString() +
+                            "\n     Now you have " + count + " tasks in the list.\n" +
+                            "____________________________________________________________");
+
+                }
                 else {
                     Task newtask = new Task(user_input);
                     System.out.println("____________________________________________________________\n  " + "added: " + user_input +
